485e676a4f087f42b2dbb2d0680484cc68946cc9
Thiago Santos <thiagoestuda9@gmail.com>
2025-08-04 12:29:53 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/thiagosantos/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.thiagosantos.workshopmongo.repository;
+
+import com.thiagosantos.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
