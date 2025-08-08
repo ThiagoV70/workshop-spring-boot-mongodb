3efbf4e4498022a93adde4380deacc979e942e0d
Thiago Santos <thiagoestuda9@gmail.com>
2025-08-08 17:01:02 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/thiagosantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/thiagosantos/workshopmongo/resources/UserResource.java
@@ -25,8 +25,6 @@ public class UserResource {
     @Autowired
     private UserService  service;
 
-
-
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
